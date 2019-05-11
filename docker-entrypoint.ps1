9dcc2a00279c5f749b495c56961278773fbc6024
Marc Hoersken <info@marc-hoersken.de>
2019-05-11 13:11:28 +0200

    Fix environment variable name DOCKER_ENV_HIDEVARS

--- docker-entrypoint.ps1
+++ docker-entrypoint.ps1
@@ -1,3 +1,3 @@
 Get-ChildItem $Env:DOCKER_ENTRYPOINT_DIR -ErrorAction Ignore | % { & $_.FullName }
-if ($env:DOCKER_ENV_HIDEVAR) { $env:DOCKER_ENV_HIDEVAR.Split() | % { Remove-Item -Force env:$_ -ErrorAction Ignore } }
+if ($env:DOCKER_ENV_HIDEVARS) { $env:DOCKER_ENV_HIDEVARS.Split() | % { Remove-Item -Force env:$_ -ErrorAction Ignore } }
 Invoke-Expression ($Args -Join " ")
